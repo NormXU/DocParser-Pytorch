714e71909b2ca63f111a18713b255d3ed2fe0571
NormXU <xunuo@datagrand.com>
2023-07-07 14:09:26 +0800

    fix "Grad strides do not match bucket view strides" when training with DDP

--- models/convnext.py
+++ models/convnext.py
@@ -1,11 +1,67 @@
 # -*- coding:utf-8 -*-
 # create: @time: 7/6/23 10:28
+
 from functools import partial
 
+import torch
 from torch import nn, Tensor
-
+from torch.nn import functional as F
+from torchvision.ops.stochastic_depth import StochasticDepth
 from typing import Any, Callable, List, Optional, Sequence, Tuple, Union
-from torchvision.models.convnext import LayerNorm2d, CNBlock
+
+
+class LayerNorm2d(nn.LayerNorm):
+    def forward(self, x: Tensor) -> Tensor:
+        x = x.permute(0, 2, 3, 1)
+        x = F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        x = x.permute(0, 3, 1, 2).contiguous()
+        return x
+
+
+class Permute(torch.nn.Module):
+    """This module returns a view of the tensor input with its dimensions permuted.
+
+    Args:
+        dims (List[int]): The desired ordering of dimensions
+    """
+
+    def __init__(self, dims: List[int]):
+        super().__init__()
+        self.dims = dims
+
+    def forward(self, x: Tensor) -> Tensor:
+        return torch.permute(x, self.dims).contiguous()
+
+
+class CNBlock(nn.Module):
+    def __init__(
+            self,
+            dim,
+            layer_scale: float,
+            stochastic_depth_prob: float,
+            norm_layer: Optional[Callable[..., nn.Module]] = None,
+    ) -> None:
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = partial(nn.LayerNorm, eps=1e-6)
+
+        self.block = nn.Sequential(
+            nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim, bias=True),
+            Permute([0, 2, 3, 1]),
+            norm_layer(dim),
+            nn.Linear(in_features=dim, out_features=4 * dim, bias=True),
+            nn.GELU(),
+            nn.Linear(in_features=4 * dim, out_features=dim, bias=True),
+            Permute([0, 3, 1, 2]),
+        )
+        self.layer_scale = nn.Parameter(torch.ones(dim, 1, 1) * layer_scale)
+        self.stochastic_depth = StochasticDepth(stochastic_depth_prob, "row")
+
+    def forward(self, input: Tensor) -> Tensor:
+        result = self.layer_scale * self.block(input)
+        result = self.stochastic_depth(result)
+        result += input
+        return result
 
 
 class CNBlockConfig:
